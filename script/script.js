a635ab2788f06a64831db54823f8317eabbdcd3f
arohapy <118235859+arohapy@users.noreply.github.com>
2024-10-27 03:07:45 +0000

    4.0 - Updated Menu items, changed navbar functionality

--- script/script.js
+++ script/script.js
@@ -1,16 +1,21 @@
+//MODULENAME: menu.html
+//WRITTEN BY AROHA KIRI 2024
+
 const MODULENAME = 'script.js';
 console.log('%c' + MODULENAME + " VERSION: 1.1", 'color: blue');
 
+
+// Called on Menu page
 function displayContent(elementId) {
     console.log('%cfunction: displayContent(' + elementId + ')', 'color: orange');
     
     var element = document.getElementById(elementId);  // Get the element by its ID
 
     if (element) {  // Ensure the element exists before trying to access it
-        if (element.style.display === "block") {
+        if (element.style.display === "grid") {
             element.style.display = "none";
         } else {
-            element.style.display = "block";
+            element.style.display = "grid";
         }
     } else {
         console.error('Element with ID ' + elementId + ' not found');
